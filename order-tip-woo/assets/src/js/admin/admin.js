18cf2c691aade92777de819c85d318a0a874fc6a
Sean Murphy <sean@iamseanmurphy.com>
2025-02-02 18:17:34 +0000

    1.5.1

---
+++ order-tip-woo/assets/src/js/admin/admin.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+(function($){
+
+    jQuery(function(){
+
+        if( jQuery('#wc_order_tip_updated_1_1').length ) {
+            jQuery('#wc_order_tip_updated_1_1').parent().parent().hide();
+        }
+    
+    });
+
+})(jQuery);
